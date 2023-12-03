aa1bed14e0e3d1a08c01dd9e10180813f511206a
Md. Shariar Islam Shuvo <shariaislamshuvo@gmail.com>
2023-12-03 23:52:05 +0600

    Add new controller and model classes for authentication and authorities

---
+++ Frontend/manga-world/src/Controller/AuthenticationController.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const USER_API_BASE_URL = "http://localhost:8080/";
+
+class AuthenticationController {
+
+}
+
+export default new AuthenticationController();
---
+++ Frontend/manga-world/src/Model/User.ts
@@ -0,0 +1,12 @@
+class User {
+    userId: number;
+    name: string;
+    email: string;
+
+    constructor(userId: number, name: string, email: string) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
+}
+export default User;
